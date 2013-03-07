3d0149a9deb659653dda9578ffb0b6d0b6af771c
peter hausel <peter_kovac11@hotmail.com>
2013-03-06 23:52:24 -0500

    adding a few missing conversions

--- src/main/scala/sedis.scala
+++ src/main/scala/sedis.scala
@@ -5,9 +5,9 @@ import redis.clients.jedis._
 trait Dress { 
   implicit def delegateToJedis(d: Wrap) = d.j 
   
-  implicit def fromJedistoScala(j: JedisCommands) = up(j) 
+  implicit def fromJedistoScala(j: Jedis) = up(j) 
 
-  class Wrap(val j: JedisCommands) {
+  class Wrap(val j: Jedis) {
     import collection.JavaConverters._
 
     def hmset(key: String, values: Map[String, String]) = {
@@ -26,6 +26,23 @@ trait Dress {
       j.smembers(key).asScala.toSet
     }
    
+    def sinter(key: String): Set[String] = {
+      j.sinter(key).asScala.toSet
+    }
+
+    def sunion(key: String): Set[String] = {
+      j.sunion(key).asScala.toSet
+    }
+
+    def sdiff(key: String): Set[String] = {
+      j.sdiff(key).asScala.toSet
+    }
+
+    def zrange(key: String, start: Long, end: Long): Set[String] = {
+      j.zrange(key, start, end).asScala.toSet
+    }
+
+
     def hkeys(key: String): Set[String] = {
       j.hkeys(key).asScala.toSet
     }
@@ -47,23 +64,28 @@ trait Dress {
     def sort(key: String):List[String] = {
       j.sort(key).asScala.toList
     }
+
+    def blpop(timeout: Int, args: String*): List[String] = {
+      j.blpop(timeout, args:_*).asScala.toList
+    }
     
+    def blpop(args: String*): List[String] = {
+      j.blpop(args:_*)
+    }
+    
+    def brpop(timeout: Int, args: String*): List[String] = {
+      j.brpop(timeout, args:_*).asScala.toList
+    }
+
+    def brpop(args: String*): List[String] = {
+      j.brpop(args:_*)
+    }
   }
-  def up(j: JedisCommands) = new Wrap(j)
+  def up(j: Jedis) = new Wrap(j)
 }
 object Dress extends Dress
 
-class Sharded(val underlying: ShardedJedis) {
 
- def withClient[T](body: Dress.Wrap => T) = {
-    try {
-      body(Dress.up(underlying))
-    } finally {
-      underlying.disconnect()
-    }
-  }
-
-}
 
 class Pool(val underlying: JedisPool) {
 
